dbc1a1f792ba1ac5252caabbf294850812170c6c
Davit Harut <davit@cincopa.com>
2025-11-12 11:43:38 +0400

    turbo json changes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { AppCincopaPlayer } from 'react-native-cincopa-video-player';
 
